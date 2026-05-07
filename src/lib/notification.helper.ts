69924d4d5a767235c6fde66ef32b167a6ff9a671
Shivam Karn <shivamkarn767@gmail.com>
2026-05-07 15:49:15 +0545

    Simple helper for notification.

---
+++ src/lib/notification.helper.ts
@@ -0,0 +1,36 @@
+import { notifications } from "../db/schema";
+import { drizzle } from "drizzle-orm/d1";
+
+type NotifType =
+  | "reaction"
+  | "comment"
+  | "comment_reply"
+  | "comment_like"
+  | "follow"
+  | "mention";
+
+type ParamsType = {
+  recipientId: string;
+  actorId: string;
+  type: string;
+  entityId: string;
+  entityType: string;
+};
+const createNotification = async (
+  db: D1Database,
+  { recipientId, actorId, type, entityId, entityType }: ParamsType,
+) => {
+  if (recipientId === actorId) return;
+  const drizzleDb = drizzle(db);
+
+  await drizzleDb.insert(notifications).values({
+    recipientId,
+    actorId,
+    type,
+    entityId,
+    entityType,
+    read: false,
+  });
+};
+
+export { createNotification };
