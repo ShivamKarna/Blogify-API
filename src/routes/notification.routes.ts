bb649c4b0746f71394c23769fb7e2fe79aebe4ab
Shivam Karn <shivamkarn767@gmail.com>
2026-05-15 12:18:43 +0545

    Completed notifications routes.

---
+++ src/routes/notification.routes.ts
@@ -0,0 +1,71 @@
+import { notificationController } from "../controllers/notification.controllers";
+import { createRoute, z, OpenAPIHono } from "@hono/zod-openapi";
+import { BindingsType, Variables } from "../lib/types";
+import { requireAuth } from "../middleware/auth.middleware";
+import { paginationWithUnreadSchema } from "./route.schemas";
+
+const notfiRouter = new OpenAPIHono<{
+  Bindings: BindingsType;
+  Variables: Variables;
+}>();
+
+notfiRouter.use("/*", requireAuth);
+
+notfiRouter.openapi(
+  createRoute({
+    method: "get",
+    path: "/",
+    tags: ["Notifications"],
+    summary: "Get all notifications",
+    request: {
+      query: paginationWithUnreadSchema,
+    },
+    responses: {
+      200: { description: "Your notifications Fetched" },
+    },
+  }),
+  notificationController.getAllNotifications,
+);
+
+notfiRouter.openapi(
+  createRoute({
+    method: "patch",
+    path: "/:id/read",
+    tags: ["Notifications"],
+    summary: "Mark Notification as Read",
+    responses: {
+      404: { description: "Notification not found" },
+      200: { description: "Notification marked as read" },
+    },
+  }),
+  notificationController.markAsRead,
+);
+
+notfiRouter.openapi(
+  createRoute({
+    method: "patch",
+    path: "/read-all",
+    tags: ["Notifications"],
+    summary: "Mark All Notifications as Read",
+    responses: {
+      200: { description: "All Notifications marked as read" },
+    },
+  }),
+  notificationController.markAllAsRead,
+);
+
+notfiRouter.openapi(
+  createRoute({
+    method: "delete",
+    path: "/:id",
+    tags: ["Notifications"],
+    summary: "Delete Notification",
+    responses: {
+      404: { description: "Notification not found" },
+      200: { description: "Notification Deleted Successfully" },
+    },
+  }),
+  notificationController.deleteNotification,
+);
+
+export { notfiRouter };
