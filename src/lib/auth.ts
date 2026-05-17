9d140fd763de68586d5a96e80e66ead8cfff4348
Shivam Karn <shivamkarn767@gmail.com>
2026-05-17 08:30:44 +0545

    Fixing state not persistent issue part 4

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -36,21 +36,9 @@ const buildAuth = (
       "http://localhost:5173",
       "https://blogify-api.shivamkarn.workers.dev",
     ],
-    account: {
-      storeStateStrategy: "cookie",
-    },
     advanced: {
-      useSecureCookies: true,
       ipAddress: {
         ipAddressHeaders: ["cf-connecting-ip"],
-      },
-      cookies: {
-        state: {
-          attributes: {
-            sameSite: "none",
-            secure: true,
-          },
-        },
       },
     },
     socialProviders: {
@@ -82,8 +70,72 @@ const buildAuth = (
       },
     },
   });
-};
 
+  //   return betterAuth({
+  //     database: drizzleAdapter(drizzleDb, {
+  //       provider: "sqlite",
+  //       schema: {
+  //         user: schema.user,
+  //         session: schema.session,
+  //         account: schema.account,
+  //         verification: schema.verification,
+  //       },
+  //     }),
+  //     secret: bindings.BETTER_AUTH_SECRET,
+  //     baseURL: bindings.BETTER_AUTH_URL,
+  //     trustedOrigins: [
+  //       "http://localhost:8787",
+  //       "http://localhost:5173",
+  //       "https://blogify-api.shivamkarn.workers.dev",
+  //     ],
+  //     account: {
+  //       storeStateStrategy: "cookie",
+  //     },
+  //     advanced: {
+  //       useSecureCookies: true,
+  //       ipAddress: {
+  //         ipAddressHeaders: ["cf-connecting-ip"],
+  //       },
+  //       cookies: {
+  //         state: {
+  //           attributes: {
+  //             sameSite: "none",
+  //             secure: true,
+  //           },
+  //         },
+  //       },
+  //     },
+  //     socialProviders: {
+  //       google: {
+  //         clientId: bindings.GOOGLE_CLIENT_ID,
+  //         clientSecret: bindings.GOOGLE_CLIENT_SECRET,
+  //       },
+  //       github: {
+  //         clientId: bindings.GITHUB_CLIENT_ID,
+  //         clientSecret: bindings.GITHUB_CLIENT_SECRET,
+  //       },
+  //       discord: {
+  //         clientId: bindings.DISCORD_CLIENT_ID,
+  //         clientSecret: bindings.DISCORD_CLIENT_SECRET,
+  //       },
+  //     },
+  //     user: {
+  //       additionalFields: {
+  //         role: {
+  //           type: "string",
+  //           defaultValue: "user",
+  //           input: false,
+  //         },
+  //         isPublic: {
+  //           type: "boolean",
+  //           defaultValue: true,
+  //           input: true,
+  //         },
+  //       },
+  //     },
+  //   });
+  // };
+};
 let cachedAuth: ReturnType<typeof buildAuth> | undefined;
 
 const getBetterAuthInstance = (
